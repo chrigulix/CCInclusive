fe2667d3e142385f4870d95f9ab2f1500285f6fa
Christoph Rudolf von Rohr <rudolf@lhep.unibe.ch>
2016-07-02 08:06:56 +0200

    Cross section calculation now subtracting backgrounds and implemented unsmearing

--- CCInclCrossSection.C
+++ CCInclCrossSection.C
@@ -37,9 +37,19 @@ bool inFV(double x, double y, double z);
 // Add two histogramms with indices First and Last and weight
 void AddHistograms(std::vector<TH1F*>& HistVector, unsigned int First, unsigned int Last, float Weight, bool EraseLast = false);
 
+// Normalize Matrix by row
+void NormMatrixByRow(TH2F* UMatrix);
 
+// Unsmearing of selected events
+void SelectionUnsmearing(TH2F*& UMatrix, TH1F*& SVector);
+
+
+// CC inlcusive cross section function (main) 
 void CCInclCrossSection()
 {
+    // Output file file type
+    std::string FileType = "pdf";
+
     // Data input file vector
     std::vector<TChain*> ChainVec;
 
@@ -57,8 +67,16 @@ void CCInclCrossSection()
 
     size_t NumberOfBins = 20;
 
-    double MCPOT = 2.3e23;
+    double MCPOT = 2.3e20;
     double DataPOT = 4.95e19;
+
+    std::vector<float> ScalingFactors;
+    ScalingFactors.push_back(1);
+    ScalingFactors.push_back(1.2300);
+    ScalingFactors.push_back(DataPOT/MCPOT);
+    ScalingFactors.push_back(DataPOT/MCPOT);
+    ScalingFactors.push_back(DataPOT/MCPOT);
+//     ScalingFactors.push_back(1);
 
     int TrkID;
     int VtxID;
@@ -131,8 +149,8 @@ void CCInclCrossSection()
     ChainVec.push_back(new TChain("anatree"));
     ChainVec.back() -> Add("/lheppc46/data/uBData/anatrees/Hist_Track_pandoraNu_Vertex_pandoraNu_prodgenie_bnb_nu_cosmic_uboone_v05_08_00_Mod.root");
 
-    ChainVec.push_back(new TChain("anatree"));
-    ChainVec.back() -> Add("/lheppc46/data/uBData/anatrees/Hist_MC_Truth_prodgenie_bnb_nu_cosmic_uboone_v05_08_00.root");
+//     ChainVec.push_back(new TChain("anatree"));
+//     ChainVec.back() -> Add("/lheppc46/data/uBData/anatrees/Hist_MC_Truth_prodgenie_bnb_nu_cosmic_uboone_v05_08_00.root");
 
     GenLabel.push_back("Data On-Beam BNB");
     GenLabel.push_back("Data Off-Beam BNBEXT");
@@ -164,12 +182,20 @@ void CCInclCrossSection()
         SelectionMomentum.back() -> GetXaxis() -> SetTitle("Muon momentum [GeV/c]");
         SelectionMomentum.back() -> GetYaxis() -> SetTitle("No. of events");
     } // loop over generation label
+    
+    // Initialize unsmearing matrices
+    UMatrixTrackRange = new TH2F("Unsmering Matrix Track Range","Unsmering Matrix Track Range",NumberOfBins,0,1036.8,NumberOfBins,0,1036.8);
+    UMatrixCosTheta = new TH2F("Unsmering Matrix CosTheta","Unsmering Matrix CosTheta",NumberOfBins,0,-1,NumberOfBins,0,-1);
+    UMatrixPhi = new TH2F("Unsmering Matrix Phi","Unsmering Matrix Phi",NumberOfBins,0,-3.142,NumberOfBins,0,3.142);
+    UMatrixMomentum = new TH2F("Unsmering Matrix Momentum","Unsmering Matrix Momentum",NumberOfBins,0,3,NumberOfBins,0,3);
 
     // Loop over all files
     for(unsigned int file_no = 0; file_no < ChainVec.size(); file_no++)
     {
+        std::cout << "----------------------------------------" << std::endl;
+
         // Reco entities for all files except truth
-        if(file_no != ChainVec.size()-1)
+        if(file_no < 3)
         {
             ChainVec.at(file_no) -> SetBranchAddress("TrackCand", &TrkID);
             ChainVec.at(file_no) -> SetBranchAddress("VertexCand", &VtxID);
@@ -230,7 +256,7 @@ void CCInclCrossSection()
             ChainVec.at(file_no) -> GetEntry(tree_index);
 
             // if there are reco products
-            if(file_no <= 2)
+            if(file_no < 3)
             {
                 // Fill histograms as usual
                 SelectionTrackRange.at(file_no) -> Fill(CalcRange(XTrackStart[TrkID],YTrackStart[TrkID],ZTrackStart[TrkID],XTrackEnd[TrkID],YTrackEnd[TrkID],ZTrackEnd[TrkID]));
@@ -254,10 +280,16 @@ void CCInclCrossSection()
                 else // if event is signal and truth
                 {
                     // Fill background histograms
-                    SelectionTrackRange.at(file_no+2) -> Fill(CalcRange(XTrackStart[TrkID],YTrackStart[TrkID],ZTrackStart[TrkID],XTrackEnd[TrkID],YTrackEnd[TrkID],ZTrackEnd[TrkID]));
-                    SelectionCosTheta.at(file_no+2) -> Fill(cos(TrackTheta[TrkID]));
-                    SelectionPhi.at(file_no+2) -> Fill(TrackPhi[TrkID]);
-                    SelectionMomentum.at(file_no+2) -> Fill(TrackMomentum[TrkID]);
+                    SelectionTrackRange.at(file_no+2) -> Fill(CalcRange(XMCTrackStart[MCTrkID],YMCTrackStart[MCTrkID],ZMCTrackStart[MCTrkID],XMCTrackEnd[MCTrkID],YMCTrackEnd[MCTrkID],ZMCTrackEnd[MCTrkID]));
+                    SelectionCosTheta.at(file_no+2) -> Fill(cos(MCTheta[MCTrkID]));
+                    SelectionPhi.at(file_no+2) -> Fill(MCPhi[MCTrkID]);
+                    SelectionMomentum.at(file_no+2) -> Fill(TrueLeptonMomentum[MCVtxID]);
+                    
+                    // Fill unsmearing matrix
+                    UMatrixTrackRange -> Fill( CalcRange(XMCTrackStart[MCTrkID],YMCTrackStart[MCTrkID],ZMCTrackStart[MCTrkID],XMCTrackEnd[MCTrkID],YMCTrackEnd[MCTrkID],ZMCTrackEnd[MCTrkID]),CalcRange(XTrackStart[TrkID],YTrackStart[TrkID],ZTrackStart[TrkID],XTrackEnd[TrkID],YTrackEnd[TrkID],ZTrackEnd[TrkID]) );
+                    UMatrixCosTheta -> Fill( cos(MCTheta[MCTrkID]),cos(TrackTheta[TrkID]) );
+                    UMatrixPhi -> Fill( MCPhi[MCTrkID],TrackPhi[TrkID] );
+                    UMatrixMomentum -> Fill( TrueLeptonMomentum[MCVtxID],TrackMomentum[TrkID] );
                 }
             } // if mc selection file
             else if(file_no == 3)
@@ -268,14 +300,156 @@ void CCInclCrossSection()
                 SelectionPhi.at(file_no+2) -> Fill(MCPhi[MCTrkID]);
                 SelectionMomentum.at(file_no+2) -> Fill(TrueLeptonMomentum[MCVtxID]);
             }
-
+            
         } // Event loop
 
         // Reset branch addresses to avoid problems
         ChainVec.at(file_no) -> ResetBranchAddresses();
 
     } // file loop
+    
+    // loop over all histograms
+    for(unsigned int hist_no = 0; hist_no < GenLabel.size(); hist_no++)
+    {
+        // Calculate standard deviation for all histograms
+        SelectionTrackRange.at(hist_no)->Sumw2();
+        SelectionCosTheta.at(hist_no)->Sumw2();
+        SelectionPhi.at(hist_no)->Sumw2();
+        SelectionMomentum.at(hist_no)->Sumw2();
+    } // histogram loop
 
+    // loop over scaleing factors 
+    for(unsigned int scale_no = 0; scale_no < ScalingFactors.size(); scale_no++)
+    {
+        // Scale histograms
+        SelectionTrackRange.at(scale_no)->Scale(ScalingFactors.at(scale_no));
+        SelectionCosTheta.at(scale_no)->Scale(ScalingFactors.at(scale_no));
+        SelectionPhi.at(scale_no)->Scale(ScalingFactors.at(scale_no));
+        SelectionMomentum.at(scale_no)->Scale(ScalingFactors.at(scale_no));
+    } // scaling loop
+    
+    // Normalize matrices by row
+    NormMatrixByRow(UMatrixTrackRange);
+    NormMatrixByRow(UMatrixCosTheta);
+    NormMatrixByRow(UMatrixPhi);
+    NormMatrixByRow(UMatrixMomentum);
+
+    // Subtract offbeam from onbeam data, overwrite onbeam- and erase offbeam histogram
+    AddHistograms(SelectionTrackRange,0,1,-1,1);
+    AddHistograms(SelectionCosTheta,0,1,-1,1);
+    AddHistograms(SelectionPhi,0,1,-1,1);
+    AddHistograms(SelectionMomentum,0,1,-1,1);
+    
+    // Subtract backgrounds from data
+    AddHistograms(SelectionTrackRange,0,2,-1);
+    AddHistograms(SelectionPhi,0,2,-1);
+    AddHistograms(SelectionCosTheta,0,2,-1);
+    AddHistograms(SelectionMomentum,0,2,-1);
+    
+    // Subtract backgrounds from mc selection and erase bgr
+    AddHistograms(SelectionTrackRange,1,2,-1,1);
+    AddHistograms(SelectionCosTheta,1,2,-1,1);
+    AddHistograms(SelectionPhi,1,2,-1,1);
+    AddHistograms(SelectionMomentum,1,2,-1,1);
+    
+    // Unsmearing loop
+    for(unsigned int hist_no = 0; hist_no < 2; hist_no++)
+    {
+        SelectionUnsmearing(UMatrixTrackRange,SelectionTrackRange.at(hist_no));
+        SelectionUnsmearing(UMatrixCosTheta,SelectionCosTheta.at(hist_no));
+        SelectionUnsmearing(UMatrixPhi,SelectionPhi.at(hist_no));
+        SelectionUnsmearing(UMatrixMomentum,SelectionMomentum.at(hist_no));
+    }
+
+    // Draw histogram
+    TCanvas *Canvas1 = new TCanvas("Range", "Range", 1400, 1000);
+    Canvas1->cd();
+    SelectionTrackRange.at(1)->SetMaximum(1.2*SelectionTrackRange.at(0)->GetBinContent(SelectionTrackRange.at(0)->GetMaximumBin()));
+    SelectionTrackRange.at(1)->SetMinimum(0.0);
+    SelectionTrackRange.at(1)->SetFillColor(46);
+    SelectionTrackRange.at(1)->Draw("E2");
+//     StackBgrTrackRange->Draw("SAME");
+    SelectionTrackRange.at(0)->SetLineWidth(2);
+    SelectionTrackRange.at(0)->SetLineColor(1);
+    SelectionTrackRange.at(0)->SetMarkerColor(1);
+    SelectionTrackRange.at(0)->Draw("SAME");
+//     LegendMC->Draw();
+//     LegendBGR->Draw();
+//     TextPreliminary.Draw();
+//     TextSelection.Draw();
+    Canvas1->SaveAs(("ScaledOn-OffBeamSelRange."+FileType).c_str());
+    
+    TCanvas *Canvas2 = new TCanvas("CosTheta", "CosTheta", 1400, 1000);
+    Canvas2->cd();
+    SelectionCosTheta.at(1)->SetMaximum(1.2*SelectionCosTheta.at(0)->GetBinContent(SelectionCosTheta.at(0)->GetMaximumBin()));
+    SelectionCosTheta.at(1)->SetMinimum(0.0);
+    SelectionCosTheta.at(1)->SetFillColor(46);
+    SelectionCosTheta.at(1)->Draw("E2");
+//     StackBgrTrackRange->Draw("SAME");
+    SelectionCosTheta.at(0)->SetLineWidth(2);
+    SelectionCosTheta.at(0)->SetLineColor(1);
+    SelectionCosTheta.at(0)->SetMarkerColor(1);
+    SelectionCosTheta.at(0)->Draw("SAME");
+//     LegendMC->Draw();
+//     LegendBGR->Draw();
+//     TextPreliminary.Draw();
+//     TextSelection.Draw();
+    Canvas2->SaveAs(("ScaledOn-OffBeamSelCosTheta."+FileType).c_str());
+    
+    TCanvas *Canvas3 = new TCanvas("Phi", "Phi", 1400, 1000);
+    Canvas3->cd();
+    SelectionPhi.at(1)->SetMaximum(1.2*SelectionPhi.at(0)->GetBinContent(SelectionPhi.at(0)->GetMaximumBin()));
+    SelectionPhi.at(1)->SetMinimum(0.0);
+    SelectionPhi.at(1)->SetFillColor(46);
+    SelectionPhi.at(1)->Draw("E2");
+//     StackBgrTrackRange->Draw("SAME");
+    SelectionPhi.at(0)->SetLineWidth(2);
+    SelectionPhi.at(0)->SetLineColor(1);
+    SelectionPhi.at(0)->SetMarkerColor(1);
+    SelectionPhi.at(0)->Draw("SAME");
+//     LegendMC->Draw();
+//     LegendBGR->Draw();
+//     TextPreliminary.Draw();
+//     TextSelection.Draw();
+    Canvas3->SaveAs(("ScaledOn-OffBeamSelPhi."+FileType).c_str());
+    
+    TCanvas *Canvas4 = new TCanvas("Momentum", "Momentum", 1400, 1000);
+    Canvas4->cd();
+    SelectionMomentum.at(1)->SetMaximum(1.2*SelectionMomentum.at(0)->GetBinContent(SelectionMomentum.at(0)->GetMaximumBin()));
+    SelectionMomentum.at(1)->SetMinimum(0.0);
+    SelectionMomentum.at(1)->SetFillColor(46);
+    SelectionMomentum.at(1)->Draw("E2");
+//     StackBgrTrackRange->Draw("SAME");
+    SelectionMomentum.at(0)->SetLineWidth(2);
+    SelectionMomentum.at(0)->SetLineColor(1);
+    SelectionMomentum.at(0)->SetMarkerColor(1);
+    SelectionMomentum.at(0)->Draw("SAME");
+//     LegendMC->Draw();
+//     LegendBGR->Draw();
+//     TextPreliminary.Draw();
+//     TextSelection.Draw();
+    Canvas4->SaveAs(("ScaledOn-OffBeamSelMomentum."+FileType).c_str());
+    
+    // Draw unsmearing matrix
+    TCanvas *Canvas5 = new TCanvas("Unsmearing Range", "Unsmearing Range", 1400, 1000);
+    Canvas5->cd();
+    UMatrixTrackRange->Draw("colz");
+    Canvas5->SaveAs(("UnsmearingRange."+FileType).c_str());
+    
+    TCanvas *Canvas6 = new TCanvas("Unsmearing CosTheta", "Unsmearing CosTheta", 1400, 1000);
+    Canvas6->cd();
+    UMatrixCosTheta->Draw("colz");
+    Canvas6->SaveAs(("UnsmearingCosTheta."+FileType).c_str());
+    
+    TCanvas *Canvas7 = new TCanvas("Unsmearing Phi", "Unsmearing Phi", 1400, 1000);
+    Canvas7->cd();
+    UMatrixPhi->Draw("colz");
+    Canvas7->SaveAs(("UnsmearingPhi."+FileType).c_str());
+    
+    TCanvas *Canvas8 = new TCanvas("Unsmearing Momentum", "Unsmearing Momentum", 1400, 1000);
+    Canvas8->cd();
+    UMatrixMomentum->Draw("colz");
+    Canvas8->SaveAs(("UnsmearingMomentum."+FileType).c_str());
 
 }
 
@@ -293,11 +467,11 @@ bool inFV(double x, double y, double z)
 void AddHistograms(std::vector<TH1F*>& HistVector, unsigned int First, unsigned int Last, float Weight, bool EraseLast)
 {
     // Check if there is something to be added
-    if (HistVector.size() >= Last && First < Last)
+    if (HistVector.size() >= Last)
     {
         // Add histograms
         HistVector.at(First) -> Add(HistVector.at(Last), Weight);
-        
+
         // Erase last histogram if flag is set
         if(EraseLast)
         {
@@ -308,5 +482,49 @@ void AddHistograms(std::vector<TH1F*>& HistVector, unsigned int First, unsigned 
     else // if nothing can be added
     {
         std::cout << "Histograms not added!" << std::endl;
+    }
+}
+
+void NormMatrixByRow(TH2F* UMatrix)
+{
+    // loop over ybins of the unsmearing matrices
+    for(unsigned int ybin = 1; ybin <= UMatrix->GetNbinsY(); ybin++)
+    {
+        float NormFact = 0;
+        
+        // loop over xbins (row)
+        for(unsigned int xbin = 1; xbin <= UMatrix->GetNbinsX(); xbin++)
+        {
+            // Add row entry to normalization factor
+            NormFact += UMatrix->GetBinContent(xbin,ybin);
+        } // xbin loop
+        
+        // loop over xbins (row)
+        for(unsigned int xbin = 1; xbin <= UMatrix->GetNbinsX(); xbin++)
+        {
+            // Normalize entire row of the matrix
+            if(NormFact) UMatrix->SetBinContent(xbin,ybin,UMatrix->GetBinContent(xbin,ybin)/NormFact) ;
+        }// xbin loop
+    }// ybin loop
+}
+
+void SelectionUnsmearing(TH2F*& UMatrix, TH1F*& SVector)
+{
+    TH1F* CloneVector = (TH1F*)SVector->Clone();
+    
+    // loop over xbins (row)
+    for(unsigned int xbin = 1; xbin <= UMatrix->GetNbinsX(); xbin++)
+    {
+        // Unsmeared bin content
+        float UnsmearedContent = 0;
+        
+        // loop over ybins of the unsmearing matrices
+        for(unsigned int ybin = 1; ybin <= UMatrix->GetNbinsY(); ybin++)
+        {
+            // Sum up the vertical contribution of the unsmeared vector
+            UnsmearedContent += CloneVector->GetBinContent(ybin)*UMatrix->GetBinContent(xbin,ybin);
+        }
+        // Fill unsmeared content into vector again
+        SVector->SetBinContent(xbin,UnsmearedContent);
     }
 }
