b6935633ea9b984d48aff6e32544b604a6ae101e
Christoph Rudolf von Rohr <crohr@lheppc34.unibe.ch>
2016-07-22 18:02:54 +0200

    Fixed beam flux bug, also introduced theta distribution

--- CCInclCrossSection.C
+++ CCInclCrossSection.C
@@ -30,8 +30,9 @@ const double borderx = 10.;
 const double bordery = 20.;
 const double borderz = 10.;
 const double Avogadro = 6.022140857e23; //mol^-1
-const double NucleonNo = 39.948; // u
-const double Density = 1.39; // g/cm^3
+const double ArMass = 39.948; // u
+const double NoNucleons = 40;
+const double Density = 1.4; // g/cm^3
 
 // Function which calculates the distance between two points
 float CalcRange(const float& x_1, const float& y_1, const float& z_1, const float& x_2, const float& y_2, const float& z_2);
@@ -52,7 +53,7 @@ void SelectionUnsmearing(TH2F*& UMatrix, TH1F*& SVector);
 // CC inlcusive cross section function (main) 
 void CCInclCrossSection()
 {
-    float NumberOfTargets = (FVx - 2*borderx) * (FVy - 2*bordery) * (FVz - 2*borderz) * Density * Avogadro;
+    float NumberOfTargets = (FVx - 2*borderx) * (FVy - 2*bordery) * (FVz - 2*borderz) * Density * Avogadro/ArMass*NoNucleons;
     
     // Output file file type
     std::string FileType = "pdf";
@@ -63,12 +64,14 @@ void CCInclCrossSection()
     // Histogram Vectors
     std::vector<TH1F*> SelectionTrackRange;
     std::vector<TH1F*> SelectionCosTheta;
+    std::vector<TH1F*> SelectionTheta;
     std::vector<TH1F*> SelectionPhi;
     std::vector<TH1F*> SelectionMomentum;
 
     // Unsemaring Matrix
     TH2F* UMatrixTrackRange;
     TH2F* UMatrixCosTheta;
+    TH2F* UMatrixTheta;
     TH2F* UMatrixPhi;
     TH2F* UMatrixMomentum;
     
@@ -82,6 +85,8 @@ void CCInclCrossSection()
 
     double MCPOT = 2.3e20;
     double DataPOT = 4.95e19;
+    
+    double IntegratedFlux;
 
     int TrkID;
     int VtxID;
@@ -138,22 +143,27 @@ void CCInclCrossSection()
     float MCPhi[5000];
     float MCEnergy[5000];
     
-    TH1D* TempNuMuFlux;
-    TH1F* NuMuFlux = new TH1F("NuMuFlux","NuMuFlux",NumberOfBins,0,3);
+    TH1D* NuMuFlux;
+//     TH1F* NuMuFlux = new TH1F("NuMuFlux","NuMuFlux",NumberOfBins,0,3);
     
     TFile* BNBFlux = new TFile("/lheppc46/data/uBData/bnbflux/numode_bnb_470m_r200.root");
     
-    TempNuMuFlux = (TH1D*) BNBFlux->Get("numu");
+    NuMuFlux = (TH1D*) BNBFlux->Get("numu");
     
-    TempNuMuFlux->Rebin(3);
+    IntegratedFlux = NuMuFlux->Integral()*4.95e19/1e20;
     
-    for(unsigned int bin_no = 1; bin_no <= NuMuFlux->GetNbinsX(); bin_no++)
-    {
-        NuMuFlux->SetBinContent(bin_no, TempNuMuFlux->GetBinContent(bin_no));
-        NuMuFlux->SetBinError(bin_no, TempNuMuFlux->GetBinError(bin_no));
-    }
+//     std::cout << TempNuMuFlux->Integral(5,NuMuFlux->GetNbinsX())*4.95e19/1e20 << std::endl;
+    std::cout << IntegratedFlux << std::endl;
     
-    NuMuFlux->Scale(DataPOT/1e20);
+//     TempNuMuFlux->Rebin(3);
+    
+//     for(unsigned int bin_no = 1; bin_no <= NuMuFlux->GetNbinsX(); bin_no++)
+//     {
+//         NuMuFlux->SetBinContent(bin_no, TempNuMuFlux->GetBinContent(bin_no));
+//         NuMuFlux->SetBinError(bin_no, TempNuMuFlux->GetBinError(bin_no));
+//     }
+    
+//     NuMuFlux->Scale(DataPOT/1e20);
     
     // Name vector
     std::vector<std::string> GenLabel;
@@ -202,6 +212,11 @@ void CCInclCrossSection()
         SelectionCosTheta.back() -> SetStats(0);
         SelectionCosTheta.back() -> GetXaxis() -> SetTitle("cos(#theta)");
         SelectionCosTheta.back() -> GetYaxis() -> SetTitle("No. of events");
+        
+        SelectionTheta.push_back(new TH1F(("#theta-Angle"+Label).c_str(),"#theta",NumberOfBins,0,3.142));
+        SelectionTheta.back() -> SetStats(0);
+        SelectionTheta.back() -> GetXaxis() -> SetTitle("#theta [rad]");
+        SelectionTheta.back() -> GetYaxis() -> SetTitle("No. of events");
 
         SelectionPhi.push_back(new TH1F(("#phi-Angle"+Label).c_str(),"#phi-Angle",NumberOfBins,-3.142,3.142));
         SelectionPhi.back() -> SetStats(0);
@@ -217,6 +232,7 @@ void CCInclCrossSection()
     // Initialize unsmearing matrices
     UMatrixTrackRange = new TH2F("Unsmering Matrix Track Range","Unsmering Matrix Track Range",NumberOfBins,0,1036.8,NumberOfBins,0,1036.8);
     UMatrixCosTheta = new TH2F("Unsmering Matrix CosTheta","Unsmering Matrix CosTheta",NumberOfBins,0,-1,NumberOfBins,0,-1);
+    UMatrixTheta = new TH2F("Unsmering Matrix Theta","Unsmering Matrix Theta",NumberOfBins,0,3.142,NumberOfBins,0,3.142);
     UMatrixPhi = new TH2F("Unsmering Matrix Phi","Unsmering Matrix Phi",NumberOfBins,0,-3.142,NumberOfBins,0,3.142);
     UMatrixMomentum = new TH2F("Unsmering Matrix Momentum","Unsmering Matrix Momentum",NumberOfBins,0,3,NumberOfBins,0,3);
 
@@ -300,6 +316,7 @@ void CCInclCrossSection()
                 // Fill histograms as usual
                 SelectionTrackRange.at(file_no) -> Fill(CalcRange(XTrackStart[TrkID],YTrackStart[TrkID],ZTrackStart[TrkID],XTrackEnd[TrkID],YTrackEnd[TrkID],ZTrackEnd[TrkID]));
                 SelectionCosTheta.at(file_no) -> Fill(cos(TrackTheta[TrkID]));
+                SelectionTheta.at(file_no) -> Fill(TrackTheta[TrkID]);
                 SelectionPhi.at(file_no) -> Fill(TrackPhi[TrkID]);
                 SelectionMomentum.at(file_no) -> Fill(TrackMomentum[TrkID]);
             }
@@ -313,6 +330,7 @@ void CCInclCrossSection()
                     // Fill background histograms
                     SelectionTrackRange.at(file_no+1) -> Fill(CalcRange(XTrackStart[TrkID],YTrackStart[TrkID],ZTrackStart[TrkID],XTrackEnd[TrkID],YTrackEnd[TrkID],ZTrackEnd[TrkID]));
                     SelectionCosTheta.at(file_no+1) -> Fill(cos(TrackTheta[TrkID]));
+                    SelectionTheta.at(file_no+1) -> Fill(TrackTheta[TrkID]);
                     SelectionPhi.at(file_no+1) -> Fill(TrackPhi[TrkID]);
                     SelectionMomentum.at(file_no+1) -> Fill(TrackMomentum[TrkID]);
                 }
@@ -321,12 +339,14 @@ void CCInclCrossSection()
                     // Fill background histograms
                     SelectionTrackRange.at(file_no+2) -> Fill(CalcRange(XMCTrackStart[MCTrkID],YMCTrackStart[MCTrkID],ZMCTrackStart[MCTrkID],XMCTrackEnd[MCTrkID],YMCTrackEnd[MCTrkID],ZMCTrackEnd[MCTrkID]));
                     SelectionCosTheta.at(file_no+2) -> Fill(cos(MCTheta[MCTrkID]));
+                    SelectionTheta.at(file_no+2) -> Fill(MCTheta[MCTrkID]);
                     SelectionPhi.at(file_no+2) -> Fill(MCPhi[MCTrkID]);
                     SelectionMomentum.at(file_no+2) -> Fill(TrueLeptonMomentum[MCVtxID]);
                     
                     // Fill unsmearing matrix
                     UMatrixTrackRange -> Fill( CalcRange(XMCTrackStart[MCTrkID],YMCTrackStart[MCTrkID],ZMCTrackStart[MCTrkID],XMCTrackEnd[MCTrkID],YMCTrackEnd[MCTrkID],ZMCTrackEnd[MCTrkID]),CalcRange(XTrackStart[TrkID],YTrackStart[TrkID],ZTrackStart[TrkID],XTrackEnd[TrkID],YTrackEnd[TrkID],ZTrackEnd[TrkID]) );
                     UMatrixCosTheta -> Fill( cos(MCTheta[MCTrkID]),cos(TrackTheta[TrkID]) );
+                    UMatrixTheta -> Fill( MCTheta[MCTrkID],TrackTheta[TrkID] );
                     UMatrixPhi -> Fill( MCPhi[MCTrkID],TrackPhi[TrkID] );
                     UMatrixMomentum -> Fill( TrueLeptonMomentum[MCVtxID],TrackMomentum[TrkID] );
                 }
@@ -336,6 +356,7 @@ void CCInclCrossSection()
                 // Fill background histograms
                 SelectionTrackRange.at(file_no+2) -> Fill(CalcRange(XMCTrackStart[MCTrkID],YMCTrackStart[MCTrkID],ZMCTrackStart[MCTrkID],XMCTrackEnd[MCTrkID],YMCTrackEnd[MCTrkID],ZMCTrackEnd[MCTrkID]));
                 SelectionCosTheta.at(file_no+2) -> Fill(cos(MCTheta[MCTrkID]));
+                SelectionTheta.at(file_no+2) -> Fill(MCTheta[MCTrkID]);
                 SelectionPhi.at(file_no+2) -> Fill(MCPhi[MCTrkID]);
                 SelectionMomentum.at(file_no+2) -> Fill(TrueLeptonMomentum[MCVtxID]);
             }
@@ -353,6 +374,7 @@ void CCInclCrossSection()
         // Calculate standard deviation for all histograms
         SelectionTrackRange.at(hist_no)->Sumw2();
         SelectionCosTheta.at(hist_no)->Sumw2();
+        SelectionTheta.at(hist_no)->Sumw2();
         SelectionPhi.at(hist_no)->Sumw2();
         SelectionMomentum.at(hist_no)->Sumw2();
     } // histogram loop
@@ -363,16 +385,18 @@ void CCInclCrossSection()
         // Scale histograms
         SelectionTrackRange.at(scale_no)->Scale(ScalingFactors.at(scale_no));
         SelectionCosTheta.at(scale_no)->Scale(ScalingFactors.at(scale_no));
+        SelectionTheta.at(scale_no)->Scale(ScalingFactors.at(scale_no));
         SelectionPhi.at(scale_no)->Scale(ScalingFactors.at(scale_no));
         SelectionMomentum.at(scale_no)->Scale(ScalingFactors.at(scale_no));
     } // scaling loop
     
     SelectionTrackRange.back()->Divide(SelectionTrackRange.at(4),SelectionTrackRange.at(5));
     SelectionCosTheta.back()->Divide(SelectionCosTheta.at(4),SelectionCosTheta.at(5));
+    SelectionTheta.back()->Divide(SelectionTheta.at(4),SelectionTheta.at(5));
     SelectionPhi.back()->Divide(SelectionPhi.at(4),SelectionPhi.at(5));
     SelectionMomentum.back()->Divide(SelectionMomentum.at(4),SelectionMomentum.at(5));
     
-    TEfficiency* EffMomentum = new TEfficiency( *SelectionMomentum.at(4),*SelectionMomentum.at(5) );
+//     TEfficiency* EffMomentum = new TEfficiency( *SelectionMomentum.at(4),*SelectionMomentum.at(5) );
     
 //     EffTrackRange.BayesDivide(SelectionTrackRange.at(3),SelectionTrackRange.at(2));
 //     EffCosTheta.BayesDivide(SelectionCosTheta.at(3),SelectionCosTheta.at(2));
@@ -382,25 +406,29 @@ void CCInclCrossSection()
     // Normalize matrices by row
     NormMatrixByRow(UMatrixTrackRange);
     NormMatrixByRow(UMatrixCosTheta);
+    NormMatrixByRow(UMatrixTheta);
     NormMatrixByRow(UMatrixPhi);
     NormMatrixByRow(UMatrixMomentum);
 
     // Subtract offbeam from onbeam data, overwrite onbeam- and erase offbeam histogram
     AddHistograms(SelectionTrackRange,0,1,-1,1);
     AddHistograms(SelectionCosTheta,0,1,-1,1);
+    AddHistograms(SelectionTheta,0,1,-1,1);
     AddHistograms(SelectionPhi,0,1,-1,1);
     AddHistograms(SelectionMomentum,0,1,-1,1);
     
     // Subtract backgrounds from data
     AddHistograms(SelectionTrackRange,0,2,-1);
-    AddHistograms(SelectionPhi,0,2,-1);
     AddHistograms(SelectionCosTheta,0,2,-1);
+    AddHistograms(SelectionTheta,0,2,-1);
+    AddHistograms(SelectionPhi,0,2,-1);
     AddHistograms(SelectionMomentum,0,2,-1);
     
     // Subtract backgrounds from mc selection and erase bgr
     AddHistograms(SelectionTrackRange,1,2,-1,1);
-    AddHistograms(SelectionCosTheta,1,2,-1,1);
     AddHistograms(SelectionPhi,1,2,-1,1);
+    AddHistograms(SelectionCosTheta,1,2,-1,1);
+    AddHistograms(SelectionTheta,1,2,-1,1);
     AddHistograms(SelectionMomentum,1,2,-1,1);
     
     // Unsmearing loop
@@ -408,29 +436,26 @@ void CCInclCrossSection()
     {
         SelectionUnsmearing(UMatrixTrackRange,SelectionTrackRange.at(hist_no));
         SelectionUnsmearing(UMatrixCosTheta,SelectionCosTheta.at(hist_no));
+        SelectionUnsmearing(UMatrixTheta,SelectionTheta.at(hist_no));
         SelectionUnsmearing(UMatrixPhi,SelectionPhi.at(hist_no));
         SelectionUnsmearing(UMatrixMomentum,SelectionMomentum.at(hist_no));
         
         SelectionTrackRange.at(hist_no)->Divide(SelectionTrackRange.back());
         SelectionCosTheta.at(hist_no)->Divide(SelectionCosTheta.back());
+        SelectionTheta.at(hist_no)->Divide(SelectionTheta.back());
         SelectionPhi.at(hist_no)->Divide(SelectionPhi.back());
         SelectionMomentum.at(hist_no)->Divide(SelectionMomentum.back());
         
-        SelectionMomentum.at(hist_no)->Divide(NuMuFlux);
-        
-        SelectionTrackRange.at(hist_no)->Scale(1/NumberOfTargets);
-        SelectionCosTheta.at(hist_no)->Scale(1/NumberOfTargets);
-        SelectionPhi.at(hist_no)->Scale(1/NumberOfTargets);
-        SelectionMomentum.at(hist_no)->Scale(1/NumberOfTargets/SelectionMomentum.at(hist_no)->GetBinWidth(1));
+        SelectionTrackRange.at(hist_no)->Scale(1/NumberOfTargets/SelectionTrackRange.at(hist_no)->GetBinWidth(1)/IntegratedFlux);
+        SelectionCosTheta.at(hist_no)->Scale(1/NumberOfTargets/SelectionCosTheta.at(hist_no)->GetBinWidth(1)/IntegratedFlux);
+        SelectionTheta.at(hist_no)->Scale(1/NumberOfTargets/SelectionTheta.at(hist_no)->GetBinWidth(1)/IntegratedFlux);
+        SelectionPhi.at(hist_no)->Scale(1/NumberOfTargets/SelectionPhi.at(hist_no)->GetBinWidth(1)/IntegratedFlux);
+        SelectionMomentum.at(hist_no)->Scale(1/NumberOfTargets/SelectionMomentum.at(hist_no)->GetBinWidth(1)/IntegratedFlux);
     }
     
     TCanvas *Canvas0 = new TCanvas("Test", "Test", 1400, 1000);
     Canvas0->cd();
-    EffMomentum->Draw("AP");
-    
-    TCanvas *Canvas0a = new TCanvas("Test1", "Test1", 1400, 1000);
-    Canvas0a->cd();
-    SelectionMomentum.back()->Draw();
+    SelectionTheta.back()->Draw();
 
     // Draw histogram
     TCanvas *Canvas1 = new TCanvas("Range", "Range", 1400, 1000);
@@ -467,8 +492,25 @@ void CCInclCrossSection()
 //     TextSelection.Draw();
     Canvas2->SaveAs(("ScaledOn-OffBeamSelCosTheta."+FileType).c_str());
     
-    TCanvas *Canvas3 = new TCanvas("Phi", "Phi", 1400, 1000);
+    TCanvas *Canvas3 = new TCanvas("Theta", "Theta", 1400, 1000);
     Canvas3->cd();
+    SelectionTheta.at(1)->SetMaximum(1.2*SelectionTheta.at(0)->GetBinContent(SelectionTheta.at(0)->GetMaximumBin()));
+    SelectionTheta.at(1)->SetMinimum(0.0);
+    SelectionTheta.at(1)->SetFillColor(46);
+    SelectionTheta.at(1)->Draw("E2");
+//     StackBgrTrackRange->Draw("SAME");
+    SelectionTheta.at(0)->SetLineWidth(2);
+    SelectionTheta.at(0)->SetLineColor(1);
+    SelectionTheta.at(0)->SetMarkerColor(1);
+    SelectionTheta.at(0)->Draw("SAME");
+//     LegendMC->Draw();
+//     LegendBGR->Draw();
+//     TextPreliminary.Draw();
+//     TextSelection.Draw();
+    Canvas3->SaveAs(("ScaledOn-OffBeamSelTheta."+FileType).c_str());
+    
+    TCanvas *Canvas4 = new TCanvas("Phi", "Phi", 1400, 1000);
+    Canvas4->cd();
     SelectionPhi.at(1)->SetMaximum(1.2*SelectionPhi.at(0)->GetBinContent(SelectionPhi.at(0)->GetMaximumBin()));
     SelectionPhi.at(1)->SetMinimum(0.0);
     SelectionPhi.at(1)->SetFillColor(46);
@@ -482,10 +524,10 @@ void CCInclCrossSection()
 //     LegendBGR->Draw();
 //     TextPreliminary.Draw();
 //     TextSelection.Draw();
-    Canvas3->SaveAs(("ScaledOn-OffBeamSelPhi."+FileType).c_str());
+    Canvas4->SaveAs(("ScaledOn-OffBeamSelPhi."+FileType).c_str());
     
-    TCanvas *Canvas4 = new TCanvas("Momentum", "Momentum", 1400, 1000);
-    Canvas4->cd();
+    TCanvas *Canvas5 = new TCanvas("Momentum", "Momentum", 1400, 1000);
+    Canvas5->cd();
     SelectionMomentum.at(1)->SetMaximum(1.2*SelectionMomentum.at(0)->GetBinContent(SelectionMomentum.at(0)->GetMaximumBin()));
     SelectionMomentum.at(1)->SetMinimum(0.0);
     SelectionMomentum.at(1)->SetFillColor(46);
@@ -499,28 +541,33 @@ void CCInclCrossSection()
 //     LegendBGR->Draw();
 //     TextPreliminary.Draw();
 //     TextSelection.Draw();
-    Canvas4->SaveAs(("ScaledOn-OffBeamSelMomentum."+FileType).c_str());
+    Canvas5->SaveAs(("ScaledOn-OffBeamSelMomentum."+FileType).c_str());
     
     // Draw unsmearing matrix
-    TCanvas *Canvas5 = new TCanvas("Unsmearing Range", "Unsmearing Range", 1400, 1000);
-    Canvas5->cd();
-    UMatrixTrackRange->Draw("colz");
-    Canvas5->SaveAs(("UnsmearingRange."+FileType).c_str());
-    
-    TCanvas *Canvas6 = new TCanvas("Unsmearing CosTheta", "Unsmearing CosTheta", 1400, 1000);
+    TCanvas *Canvas6 = new TCanvas("Unsmearing Range", "Unsmearing Range", 1400, 1000);
     Canvas6->cd();
-    UMatrixCosTheta->Draw("colz");
-    Canvas6->SaveAs(("UnsmearingCosTheta."+FileType).c_str());
+    UMatrixTrackRange->Draw("colz");
+    Canvas6->SaveAs(("UnsmearingRange."+FileType).c_str());
     
-    TCanvas *Canvas7 = new TCanvas("Unsmearing Phi", "Unsmearing Phi", 1400, 1000);
+    TCanvas *Canvas7 = new TCanvas("Unsmearing CosTheta", "Unsmearing CosTheta", 1400, 1000);
     Canvas7->cd();
-    UMatrixPhi->Draw("colz");
-    Canvas7->SaveAs(("UnsmearingPhi."+FileType).c_str());
+    UMatrixCosTheta->Draw("colz");
+    Canvas7->SaveAs(("UnsmearingCosTheta."+FileType).c_str());
     
-    TCanvas *Canvas8 = new TCanvas("Unsmearing Momentum", "Unsmearing Momentum", 1400, 1000);
+    TCanvas *Canvas8 = new TCanvas("Unsmearing Theta", "Unsmearing Theta", 1400, 1000);
     Canvas8->cd();
+    UMatrixTheta->Draw("colz");
+    Canvas8->SaveAs(("UnsmearingTheta."+FileType).c_str());
+    
+    TCanvas *Canvas9 = new TCanvas("Unsmearing Phi", "Unsmearing Phi", 1400, 1000);
+    Canvas9->cd();
+    UMatrixPhi->Draw("colz");
+    Canvas9->SaveAs(("UnsmearingPhi."+FileType).c_str());
+    
+    TCanvas *Canvas10 = new TCanvas("Unsmearing Momentum", "Unsmearing Momentum", 1400, 1000);
+    Canvas10->cd();
     UMatrixMomentum->Draw("colz");
-    Canvas8->SaveAs(("UnsmearingMomentum."+FileType).c_str());
+    Canvas10->SaveAs(("UnsmearingMomentum."+FileType).c_str());
 
 }
 
