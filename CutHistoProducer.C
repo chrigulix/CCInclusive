6d8ebeccb28460266387ee86f933bd449a5aac46
Christoph Rudolf von Rohr <rudolf@lhep.unibe.ch>
2022-02-22 17:09:10 +0100

    Cut histor producer now makes nice plots

--- CutHistoProducer.C
+++ CutHistoProducer.C
@@ -16,13 +16,14 @@
 void ReverseScan(TH1F* RawHisto);
 void Scan(TH1F* RawHisto);
 void CalculateSign(TH1F* SignalHist, TH1F* BgrHist);
+void MovingAverage(TH1F* SignalHist, int AverageWidth);
 
-void CutHistoProducer()
+void DrawCutOptimisation()
 {
-    std::string Folder = "CCInclusiveNote";
-//     std::string Folder = "ThesisSelection";
+//     std::string Folder = "/home/christoph/anatrees/CCInclusiveNote";
+    std::string Folder = "./CCInclFiles";
     
-    TFile* InputFile = new TFile(("/lheppc46/data/uBData/anatrees/"+Folder+"/Cut_Optimizer_prodgenie_bnb_nu_cosmic_uboone_v05_08_00_Mod.root").c_str());
+    TFile* InputFile = new TFile((Folder+"/Cut_Optimizer_prodgenie_bnb_nu_cosmic_uboone_v05_08_00_Mod.root").c_str());
 //     TFile* InputFile = new TFile("rootfiles/Cut_Optimizer_prodgenie_bnb_nu_cosmic_uboone_v05_08_00_Mod.root");
     
     TH1F* FlashSignal = (TH1F*)InputFile->Get("FlashSignal");
@@ -76,6 +77,8 @@ void CutHistoProducer()
     CalculateSign(FlashDistSignal,FlashDistBGR);
     CalculateSign(TrackRangeSignal,TrackRangeBGR);
     
+//     MovingAverage(TrackRangeSignal,5);
+    
     TCanvas* C0 = new TCanvas("Flash", "Flash", 1400, 1000);
     C0->cd();
     FlashSignal->Draw();
@@ -84,29 +87,65 @@ void CutHistoProducer()
     C1->cd();
     VtxDistanceSignal->Draw();
     
-    TCanvas* C1a = new TCanvas("True Vertex Dist", "True Vertex Dist", 1400, 1000);
-    C1a->cd();
+    TCanvas* C2 = new TCanvas("True Vertex Dist", "True Vertex Dist", 1400, 1000);
+    C2->cd();
     TrueVtxDistanceSignal->Draw();
     
-    TCanvas* C2 = new TCanvas("Vertex Pos X", "Vertex Pos X", 1400, 1000);
-    C2->cd();
+    TCanvas* C3 = new TCanvas("Vertex Pos X", "Vertex Pos X", 1400, 1000);
+    C3->cd();
     XVtxPosSignal->Draw();
     
-    TCanvas* C3 = new TCanvas("Vertex Pos Y", "Vertex Pos Y", 1400, 1000);
-    C3->cd();
-    YVtxPosSignal->Draw();
+    TLine* YPosLine = new TLine(20.0,0.0,20.0,84.0);
+    YPosLine -> SetLineWidth(4);
+    YPosLine -> SetLineColor(12);
+    TLatex *YPosText = new TLatex(22.0,40.0,"Max at 20 cm");
     
-    TCanvas* C4 = new TCanvas("Vertex Pos Z", "Vertex Pos Z", 1400, 1000);
+    TCanvas* C4 = new TCanvas("Vertex Pos Y", "Vertex Pos Y", 1400, 1000);
     C4->cd();
+    YVtxPosSignal -> SetLineColor(38);
+    YVtxPosSignal -> SetLineWidth(4);
+    YVtxPosSignal -> SetTitle("Fiducial Cut Optimisation");
+    YVtxPosSignal -> GetXaxis() -> SetRangeUser(0,100);
+    YVtxPosSignal -> GetYaxis() -> SetRangeUser(0,100);
+    YVtxPosSignal -> GetXaxis() -> SetTitle("Y-Axis Fiducial Cut Value [cm]");
+    YVtxPosSignal -> GetYaxis() -> SetTitle("S / #sqrt{S + B} [ ]");
+    YVtxPosSignal -> GetYaxis() -> SetTitleOffset(0.95);
+    YVtxPosSignal -> Draw();
+    YPosLine -> Draw("SAME");
+    YPosText -> Draw("SAME");
+    gPad->RedrawAxis();
+    C4 -> SaveAs("../images/FirstCCInclusive/CutOptFiducialY.pdf");
+    
+    TCanvas* C5 = new TCanvas("Vertex Pos Z", "Vertex Pos Z", 1400, 1000);
+    C5->cd();
     ZVtxPosSignal->Draw();
     
-    TCanvas* C5 = new TCanvas("Flash Dist", "Flash Dist", 1400, 1000);
-    C5->cd();
+    TCanvas* C6 = new TCanvas("Flash Dist", "Flash Dist", 1400, 1000);
+    C6->cd();
     FlashDistSignal->Draw();
     
-    TCanvas* C6 = new TCanvas("Track Range", "Track Range", 1400, 1000);
-    C6->cd();
-    TrackRangeSignal->Draw();
+    TLine* LengthLine = new TLine(75.0,0.0,75.0,60.0);
+    LengthLine -> SetLineWidth(4);
+    LengthLine -> SetLineColor(12);
+    TLatex *LengthText = new TLatex(80.0,30.0,"Max at 75 cm");
+    
+    TCanvas* C7 = new TCanvas("Track Range", "Track Range", 1400, 1000);
+    C7->cd();
+    TrackRangeSignal -> Rebin(5);
+    TrackRangeSignal -> Scale(0.2);
+    TrackRangeSignal -> SetLineColor(38);
+    TrackRangeSignal -> SetLineWidth(4);
+    TrackRangeSignal -> SetTitle("Track Range Cut Optimisation");
+    TrackRangeSignal -> GetXaxis() -> SetRangeUser(0,500);
+    TrackRangeSignal -> GetYaxis() -> SetRangeUser(0,70);
+    TrackRangeSignal -> GetXaxis() -> SetTitle("Track Range Cut Value [cm]");
+    TrackRangeSignal -> GetYaxis() -> SetTitle("S / #sqrt{S + B} [ ]");
+    TrackRangeSignal -> GetYaxis() -> SetTitleOffset(0.95);
+    TrackRangeSignal->Draw("HIST");
+    LengthLine -> Draw("SAME");
+    LengthText -> Draw("SAME");
+    gPad->RedrawAxis();
+    C7 -> SaveAs("../images/FirstCCInclusive/CutOptTrackRange.pdf");
 }
 
 void ReverseScan ( TH1F* RawHisto )
@@ -145,5 +184,31 @@ void CalculateSign ( TH1F* SignalHist, TH1F* BgrHist )
     
     SignalHist->Divide(BgrHist);
     
-    std::cout << "Cut maximum bin: " << SignalHist->GetBinCenter(SignalHist->GetMaximumBin()) << std::endl;
+    std::cout << "Cut maximum bin: " << SignalHist->GetBinCenter(SignalHist->GetMaximumBin()) << " Maximum Value: " << SignalHist->GetBinContent(SignalHist->GetMaximumBin()) << std::endl;
+}
+
+void MovingAverage(TH1F* SignalHist, int AverageWidth)
+{
+    unsigned int NumberOfBins = SignalHist -> GetNbinsX();
+    
+    std::cout << NumberOfBins << std::endl;
+    
+    TH1F* HistCopy = (TH1F*) SignalHist -> Clone();
+    
+    for(unsigned int bin_no = 1; bin_no <= NumberOfBins; bin_no++)
+    {
+        if((bin_no - AverageWidth) > 0 && (bin_no + AverageWidth) <= NumberOfBins)
+        {
+            float Filtered = 0.0;
+            unsigned int BinCount = 0;
+            for(int filter_bin = -AverageWidth; filter_bin <= AverageWidth; filter_bin++)
+            {
+                Filtered += HistCopy -> GetBinContent(filter_bin+bin_no);
+                BinCount++;
+            }
+            Filtered /= (float)BinCount;
+            
+            SignalHist -> SetBinContent(bin_no, Filtered);
+        }
+    }
 }
